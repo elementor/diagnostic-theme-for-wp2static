da737ad57f7f0609906c50bba908905a60a2b684
Leon Stafford <lionhive@gmail.com>
2018-12-19 01:49:05 +0700

    sample chart

--- functions.php
+++ functions.php
@@ -8,6 +8,24 @@ function wp2static_diagnostics_script_loader() {
         false, // will append current WP version
         false
     );
+
+    wp_enqueue_script(
+        'wp2static-chart-bundle',
+        // https://www.chartjs.org/samples/latest/charts/line/basic.html
+        get_template_directory_uri() . '/js/Chart.bundle.js',
+        array (),
+        false, // will append current WP version
+        false
+    );
+
+    wp_enqueue_script(
+        'wp2static-chart-utils',
+        // https://www.chartjs.org/samples/latest/charts/line/basic.html
+        get_template_directory_uri() . '/js/utils.js',
+        array (),
+        false, // will append current WP version
+        false
+    );
 }
 
 add_action( 'wp_enqueue_scripts', 'wp2static_diagnostics_script_loader' );
